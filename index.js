06101bb392d519d750e32466e7486d8ade83a6eb
bizzy1227 <archstart1227@gmail.com>
2021-02-08 20:20:53 +0200

    adde package files

--- index.js
+++ index.js
@@ -3,24 +3,22 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 const fs = require("fs");
 const chrome = require('selenium-webdriver/chrome');
 let opts = new chrome.Options();
-const loginURL = 'https://www.tenjin.com/login';
+const loginURL = 'https://www.tenjin.com';
 const dataURL = 'https://www.tenjin.com/dashboard/data_exporter?utf8=%E2%9C%93&start_date=2021-01-09&end_date=2021-02-08&granularity=daily&group_by=app_id&metrics%5B%5D=spend&metrics%5B%5D=installs&metrics%5B%5D=clicks&metrics%5B%5D=impressions&metrics%5B%5D=cpi&metrics%5B%5D=ctr&metrics%5B%5D=cvr&metrics%5B%5D=tcpi&report_type=user_acquisition&button=';
 
 
 
 const parseTenjin = async function() {
-    opts
-        .addArguments(['--ignore-certificate-errors', '--ignore-ssl-errors'])
-        .setUserPreferences({ 'download.default_directory': './' })
+    opts.addArguments(['--ignore-certificate-errors', '--ignore-ssl-errors'])
+
     let driver = await new Builder().forBrowser('chrome')
-    .setChromeOptions(new chrome.Options().setUserPreferences({ "download.default_directory": './' }))
+    .setChromeOptions(opts)
     .build();
     
     try {
         await driver.get(dataURL);
 
         let form = await driver.findElement(By.css('form#login-form'));
-        console.log(form);
 
         let email = await form.findElement(By.css('input#login-input-email'));
         await email.sendKeys('archstar1227@gmail.com');
